15d475a735ffbdcc7369ae8f426a241734192217
Venkata Mahesh Babu Korrapati <korrapav@mail.gvsu.edu>
2025-01-12 19:24:12 -0600

    Search service

--- search/src/main/java/com/example/search/controller/SearchController.java
+++ search/src/main/java/com/example/search/controller/SearchController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class SearchController {
@@ -14,4 +16,10 @@ public class SearchController {
         //TODO
         return new ResponseEntity<>("this is search service", HttpStatus.OK);
     }
+
+    @GetMapping("api/universities")
+    public String getMethodName(@RequestParam String country) {
+        return Universitycontroller.getUniversitiesByCountry(country);
+    }
+    
 }
